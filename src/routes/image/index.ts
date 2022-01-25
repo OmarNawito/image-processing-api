3c79cbec20f814f6283f6187426f72e3db3a1e95
Omar Nawito <omar.nawito@linkdev.com>
2022-01-25 20:30:38 +0200

    handel if image not exist

--- src/routes/image/index.ts
+++ src/routes/image/index.ts
@@ -7,28 +7,34 @@ import { fileExisits, sharpResize } from '../../utilities';
 const imageRoutes = express.Router();
 imageRoutes.use(bodyParser.json())
 
-imageRoutes.get('/resize', async(req: Request, res: Response, next: NextFunction) => {
-    const {h, w, filename} = req.query;
-    console.log('h', h);
-    console.log('w', w);
-    const width: number | null = w ? parseInt(w as string, 10): null;
-    const height: number | null = h ? parseInt(h as string, 10): null;
+imageRoutes.get('/resize', async (req: Request, res: Response, next: NextFunction) => {
+    const { filename, height, width } = req.query;
+    const w: number | null = height ? parseInt(width as string, 10) : null;
+    const h: number | null = width ? parseInt(width as string, 10) : null;
     const f: string = filename as unknown as string;
-    console.log('f', f);
-
     try {
         const imagePath = `${f}${w}x${h}.jpg`;
         console.log('imagePath', imagePath);
         const resizePath = `./public/${f}${w}x${h}.jpg`;
         const imagePathExists = await fileExisits(path.join("public", imagePath));
         console.log('imagePathExists', imagePathExists);
-        if(imagePathExists) {
+        if (imagePathExists) {
             res.sendFile(`/${imagePath}`, { root: path.join("./public") });
-        }else {
-
+        } else {
+            const response = await sharpResize(f, h, w);
+            response.toFile(resizePath, (error: Error) => {
+                if (error) {
+                    res.status(403).send({
+                        ok: "failed",
+                        message: error.message,
+                    });
+                } else {
+                    res.sendFile(`/${imagePath}`, { root: path.join("./public") });
+                }
+            });
         }
     } catch (error) {
-        
+        console.log(e);
     }
 });
 
