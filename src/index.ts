93903e57b293d6a39d0565c53ce94c0a56aed511
Omar Nawito <omar.nawito@linkdev.com>
2022-01-25 19:46:00 +0200

    install eslint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express, { Request, Response, Application } from 'express';
 
 const app: Application = express();
-const port: number = 3000;
+const port = 3000;
 
 app.get('/', async (_req: Request, res: Response): Promise<void> => {
   res.send('Image processing started');
