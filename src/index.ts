e70cb5d84832349751b8be96944cad527c6064eb
Omar Nawito <omar.nawito@linkdev.com>
2022-01-25 19:42:10 +0200

    install prettier

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,12 @@
-import express, {Request, Response, Application} from 'express'
+import express, { Request, Response, Application } from 'express';
 
 const app: Application = express();
 const port: number = 3000;
 
 app.get('/', async (_req: Request, res: Response): Promise<void> => {
-    res.send('Image processing started');
-}
-);
+  res.send('Image processing started');
+});
 
 app.listen(port, () => {
-    console.log(`server started on port: ${port}`);
-})
+  console.log(`server started on port: ${port}`);
+});
