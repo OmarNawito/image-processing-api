d81c414b6d92ca3d545b7e74f00d911c5e4ec1ab
omarNawito <o_mohsen@hotmail.com>
2022-01-26 17:40:28 +0200

    add another two tests

--- src/tests/routes/image/ImageProcessingSpec.ts
+++ src/tests/routes/image/ImageProcessingSpec.ts
@@ -13,4 +13,19 @@ describe("Resize Controller", () => {
         expect(response.body.message).toBe("Input file is missing");
         done();
       });
+
+      it("should return an error message if width is not a number", async (done) => {
+        const response = await request.get(
+          "/api/img/resize?filename=test&width=hello&height=200"
+        );
+        expect(response.text).toBe("Please provide a positive numerical value for the 'width' query segment.");
+        done();
+      });
+      it("should return an error message if height is not a number", async (done) => {
+        const response = await request.get(
+          "/api/img/resize?filename=test&width=200&height=hello"
+        );
+        expect(response.text).toBe("Please provide a positive numerical value for the 'height' query segment.");
+        done();
+      });
 });
